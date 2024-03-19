c3580b224b62cbec9249b22bb9bc5a9402851166
Hansong-Zhang <73114816+Hansong-Zhang@users.noreply.github.com>
2024-03-19 22:47:16 +0800

    Add files via upload

---
+++ scripts/distill_ipc1.sh
@@ -0,0 +1,10 @@
+
+
+
+for d in "imagenette" "imagewoof" "imagemeow" "imagesquawk"
+do
+    for sel_prob in 0.1 0.3 0.5 0.7 0.9
+    do
+        python3 ../distill/DATM_SAM.py --cfg ../configs/ImageNet-Subsets/IPC1.yaml --subset $d --sel_p $sel_prob
+    done
+done
---
+++ scripts/run_buffer_imagewoof.sh
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+python3 ../buffer/buffer_FTD.py --dataset=ImageNet --subset=imagewoof --model=ConvNetD5  \
+                                --num_experts=100                                         \
+                                --lr_teacher=0.01                                         \
+                                --batch_train=128                                         \
+                                --batch_real=128                                          \
+                                --dsa=True                                                \
+                                --dsa_strategy=color_crop_cutout_flip_scale_rotate        \
+                                --train_epochs=60                                         \
+                                --mom=0 --l2=0                                                    \
+                                --save_interval=10                                        \
+                                --rho_max=0.01 --rho_min=0.01 --alpha=0.3  \
+                                --adaptive=True \
+                                --data_path=/home/wangkai/big_space/datasets/imagenet/ \
+                                --buffer_path=/home/wangkai/big_space/hs_zhang/Z_demo/DATM/buffer_storage/ \
+                                --logs_dir=/home/wangkai/big_space/hs_zhang/Z_demo/DATM/buffer_logs/
